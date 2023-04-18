5ebaac22455979a93489f07cc465ce53af9a111e
Iasson Grabowski <iasson@risclog.com>
2023-04-18 11:42:19 +0200

    more Advanced Decorators

--- 2023-04-18/10_30_advanced.py
+++ 2023-04-18/10_30_advanced.py
@@ -60,8 +60,6 @@ c()
 print(c.count)
 print(callable(c))
 
-from functools import wraps
-
 class Say:
     def __init__(self, text) -> None:
         self.text = text
@@ -80,3 +78,100 @@ def add(a,b):
 
 print(add(4,5))
 
+
+
+# Argument check
+
+import functools
+
+def check(*argtypes):
+    """Function argument type checker.
+    """
+
+    def _check(func):
+        """Takes the function.
+        """
+
+        @functools.wraps(func)
+        def __check(*args):
+            """Takes the arguments
+            """
+            if len(args) != len(argtypes):
+                msg = f'Expected {len(argtypes)} but got {len(args)} arguments'
+                raise TypeError(msg)
+            for arg, argtype in zip(args, argtypes):
+                if not isinstance(arg, argtype):
+                    msg = f'Expected {argtypes} but got '
+                    msg += f'{tuple(type(arg) for arg in args)}'
+                    raise TypeError(msg)
+            return func(*args)
+        return __check
+    return _check
+
+
+@check(float, float)
+def add(a, b):
+    return a + b
+
+print(add(3., 4.))
+
+
+# Registering
+"""A function registry.
+"""
+
+import functools
+
+registry = {}
+
+
+def register_at_call(name):
+    """Register the decorated function at call time.
+    """
+
+    def _register(func):
+        """Takes the function.
+        """
+
+        @functools.wraps(func)
+        def __register(*args, **kwargs):
+            """Takes the arguments.
+            """
+            registry.setdefault(name, []).append(func)
+            return func(*args, **kwargs)
+        return __register
+    return _register
+
+
+def register_at_def(name):
+    """Register the decorated function at definition time.
+    """
+
+    def _register(func):
+        """Takes the function.
+        """
+        registry.setdefault(name, []).append(func)
+
+        return func
+    return _register
+
+
+@register_at_call('simple')
+def add(a, b):
+    return a + b
+
+print(registry)
+add(7,4)
+print(registry)
+registry.clear()
+print(registry)
+
+@register_at_def('complex')
+def add(a, b):
+    return a + b
+
+print(registry)
+add(7,4)
+print(registry)
+registry.clear()
+print(registry)
