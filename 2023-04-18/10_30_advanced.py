e5750822370b306cd9c2db6f24810ba3e6f1c3ec
Iasson Grabowski <iasson@risclog.com>
2023-04-18 11:52:15 +0200

    Class  Decorators

--- 2023-04-18/10_30_advanced.py
+++ 2023-04-18/10_30_advanced.py
@@ -175,3 +175,63 @@ add(7,4)
 print(registry)
 registry.clear()
 print(registry)
+
+
+# Class Decorators
+
+def mark(cls):
+    cls.new_attr = 100
+    return cls
+
+@mark
+class A:
+    pass
+print(A.new_attr)
+
+# OR
+class A:
+    pass
+A = mark(A)
+
+print(A.new_attr)
+
+
+# Method name check (Class Decorator)
+
+def check_name_length(max_len=30):
+    """Check method name length.
+
+    Raises a `NameError` if one method name of a decoratoed class is
+    longer than `max_len`.
+    """
+    def _check_name_length(cls):
+        for name, obj in cls.__dict__.items():
+            if callable(obj) and len(name) > max_len:
+                msg = (f'name `{name}` too long,\n  ' + len('NameError') * ' ' +
+                       f'found {len(name)} characters, only {max_len} are allowed')
+                raise NameError(msg)
+        return cls
+    return _check_name_length
+
+"""
+@check_name_length()
+class B:
+    def method(self):
+        pass
+    
+    def method1234568864553454234234141321231231231231(self):
+        pass
+
+Traceback (most recent call last):
+  File "/Users/ig/develop/pycon2023/2023-04-18/10_30_advanced.py", line 218, in <module>
+    class B:
+  File "/Users/ig/develop/pycon2023/2023-04-18/10_30_advanced.py", line 212, in _check_name_length
+    raise NameError(msg)
+NameError: name `method1234568864553454234234141321231231231231` too long,
+           found 46 characters, only 30 are allowed"""
+           
+           
+@check_name_length()
+class B:
+    def method(self):
+        pass
